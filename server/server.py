c95ff42e7eb4182643086dc086c3a6e1bd53374e
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-10-31 22:59:46 -0500

    Able to fetch data from the WeatherAPI.
    The code is able to fetch data but you will need to get your own credentials.

--- server/server.py
+++ server/server.py
@@ -1,14 +1,24 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/", methods=['GET'])
 def home():
-  return jsonify({
-    'message': "Hello World!"
-  })
+  url = "https://weatherapi-com.p.rapidapi.com/current.json"
+
+  querystring = {"q":"30.6, -96.3"} # College Station, TX Latitude and Longitude Coordinates
+
+  headers = {
+    "X-RapidAPI-Key": "",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+  }
+
+  response = requests.get(url, headers=headers, params=querystring)
+  
+  return response.json()
 
 if __name__ == "__main__":
   app.run(debug=True, port=8080)
