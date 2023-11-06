14bc0bc3c37d99eb24c4494c871ea661512b8548
jacobnguyen-cs <jacobnguyen726@yahoo.com>
2023-11-06 14:11:30 -0600

    Implemented WeatherAPI With Geolocation.
    Implemented WeatherAPI using Geolocation to get weather at current location. If you parse the json response then you are able to find the weather code for specific weather conditions.

--- server/server.py
+++ server/server.py
@@ -1,24 +1,43 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-import requests
+from flask import Flask, jsonify        # pip install 
+from flask_cors import CORS             # pip install flask_cors
+from geopy.geocoders import Nominatim   # pip install geopy
+import requests                         # pip install requests
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['GET'])
-def home():
-  url = "https://weatherapi-com.p.rapidapi.com/current.json"
+### SAMPLE CODE For HTTP Requests ###
+# @app.route("/", methods=['GET'])
+# def home():
+#   url = "https://weatherapi-com.p.rapidapi.com/current.json"
+#   loc = Nominatim(user_agent="GetLoc")
+#   getLoc = loc.geocode("College Station")
+#   coordinates = "%.2f, %.2f" % (getLoc.latitude, getLoc.longitude)
+#   querystring = {"q": coordinates} # College Station, TX Latitude and Longitude Coordinates
+#   headers = {
+#     "X-RapidAPI-Key": "cdc0c19252msh6449b7b29d1bb94p1dcf61jsn236a7bab7b74",
+#     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+#   }
+#   response = requests.get(url, headers=headers, params=querystring)
+#   return response.json()
 
-  querystring = {"q":"30.6, -96.3"} # College Station, TX Latitude and Longitude Coordinates
+### Get coordinates ###
+loc = Nominatim(user_agent="GetLoc")
+getLoc = loc.geocode("College Station")
+coordinates = "%.2f, %.2f" % (getLoc.latitude, getLoc.longitude)
 
-  headers = {
-    "X-RapidAPI-Key": "",
-    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
-  }
+### GET Request to WeatherAPI with  coordinates ###
+url = "https://weatherapi-com.p.rapidapi.com/current.json"
+querystring = {"q": coordinates} # College Station, TX Latitude and Longitude Coordinates
+headers = {
+  "X-RapidAPI-Key": "cdc0c19252msh6449b7b29d1bb94p1dcf61jsn236a7bab7b74",
+  "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+}
+response = requests.get(url, headers=headers, params=querystring)
 
-  response = requests.get(url, headers=headers, params=querystring)
-  
-  return response.json()
+# print(response.json())
+print(response.json()["current"]["condition"])
+
 
 if __name__ == "__main__":
   app.run(debug=True, port=8080)
